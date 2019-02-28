a685dd3832c00716a7632d50a20d878a9525728e
Amr-ElNahas <amrtea.edu@gmail.com>
2019-03-01 01:25:41 +0200

    modified index

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-/*** npm modules ***/
+/** * npm modules ***/
 const express = require('express')
 const port = 3000
 
-/*** project modules ***/
+/** * project modules ***/
 // example: const router = require('router/api/company')
-
-/*** global constants ***/
+const entityEmployeeRouter = require('./routes/api/EntityEmployee')
+/** * global constants ***/
 const app = express()
 
-/*** adding controllers/routers ***/
+/** * adding controllers/routers ***/
 // example: app.use('/api/company', company)
+app.use('/api/EntityEmployee', entityEmployeeRouter)
 
-
-/*** Adding temporary index page ***/
-app.get('/', (req, res)=>{res.send("<h1>WillCodeToLive</h1>\n<h3>Index Page<h3>")})
-/*** Custom routing for wrong requests ***/
+/** * Adding temporary index page ***/
+app.get('/', (req, res) => { res.send('<h1>WillCodeToLive</h1>\n<h3>Index Page<h3>') })
+/** * Custom routing for wrong requests ***/
 app.use((req, res) => {
-    res.status(404).send({err: 'Obi-Wan: You don\'t need to see this page...'})
+  res.status(404).send({ err: 'Obi-Wan: You don\'t need to see this page...' })
 })
-/*** Listening on serverport ***/
-app.listen(port, ()=>console.log(`Server up. Listening on port ${port}`))
+/** * Listening on serverport ***/
+app.listen(port, () => console.log(`Server up. Listening on port ${port}`))
