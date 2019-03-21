b0234bdcf94128f8a7f8667518c4a515a0116ee8
Amr-ElNahas <amrtea.edu@gmail.com>
2019-03-21 20:43:12 +0200

    changed entityemployee to admin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 /** * npm modules ***/
 const express = require('express')
 const port = 3000
+const mongoose = require('mongoose')
 
 /** * project modules ***/
 // example: const router = require('router/api/company')
@@ -11,30 +12,32 @@ const tasks = require('./routes/api/task')
 const investors = require('./router/api/investors')
 const sscForm = require('./routes/api/sscForm')
 const spcForm = require('./routes/api/spcForm')
-const cases = require('./routes/api/cases');
-const entityEmployeeRouter = require('./routes/api/EntityEmployee')
-const external_entities=require('./routes/api/external_entities')
+const cases = require('./routes/api/cases')
+const adminRouter = require('./routes/api/admin')
+const external_entities = require('./routes/api/external_entities')
 const companyRouter = require('./routes/api/company')
 const Lawyer = require('./routes/api/lawyer')
 /** * global constants ***/
 const app = express()
-app.use(express.json());
-
+app.use(express.json())
 
 /** * adding controllers/routers ***/
 // example: app.use('/api/company', company)
 
-app.use('/api/Lawyer',Lawyer)
+app.use('/api/Lawyer', Lawyer)
 app.use('/api/reviewer', reviewers)
 app.use('/api/tasks', tasks)
 app.use('/api/investors', investors)
 app.use('/api/company', companyRouter)
-app.use('/api/sscform',sscForm)
-app.use('/api/spcform',spcForm)
-app.use('/api/cases',cases);
-app.use('/api/EntityEmployee', entityEmployeeRouter)
-app.use('/api/ExternalEntity',external_entities)
+app.use('/api/sscform', sscForm)
+app.use('/api/spcform', spcForm)
+app.use('/api/cases', cases)
+app.use('/api/admin', adminRouter)
+app.use('/api/ExternalEntity', external_entities)
 
+/** * Connectiong to db ***/
+const db = require('./config/keys').mongoURI
+mongoose.connect(db).then(() => console.log('Connected to MongoDB')).catch(err => console.log(err))
 
 /** * Adding temporary index page ***/
 app.get('/', (req, res) => { res.send('<h1>WillCodeToLive</h1>\n<h3>Index Page<h3>') })
@@ -42,5 +45,5 @@ app.get('/', (req, res) => { res.send('<h1>WillCodeToLive</h1>\n<h3>Index Page<h
 app.use((req, res) => {
   res.status(404).send({ err: 'Obi-Wan: You don\'t need to see this page...' })
 })
-/*** Listening on serverport ***/
+/** * Listening on serverport ***/
 app.listen(port, () => console.log(`Server up. Listening on port ${port}`))
