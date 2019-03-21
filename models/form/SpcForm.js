545711573d7b206a55c344e536ab931763e8b199
abirgamal <abiirrgamal@gmail.com>
2019-03-21 16:26:38 +0200

    Mongo Models

--- models/form/SpcForm.js
+++ models/form/SpcForm.js
@@ -1,75 +1,117 @@
-// npm imports
-const uuid = require('uuid')
-const joi = require('joi')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
-// Global constants
-// Lists
-const regulatingLawList = ["law1", "law2"]
-const companyLegalFormList = ["form1", "form2"]
-const governerateList = ["gov1", "gov2"]
-const cityList = ["city1", "city2"]
-const currencyList = ["cur1", "cur2"]
-const genderList = ["g1", "g2"]
-const nationalityList = ["n1", "n2"]
-const idTypeList = ["id1", "id2"]
+const FormSchema = new Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
 
-// Schemas
-const companyNameSchema =  {
-    arabic : joi.string().min(3).required(),
-    english : joi.string().min(3)
-}
-
-const hqInfoSchema = {
-    governerate : joi.any().valid(governerateList).required(),
-    city : joi.any().valid(cityList).required(),
-    address : joi.string().min(3).required(),
-    telephone : joi.number(),
-    fax : joi.number()
-}
-
-const investorInfoSchema = {
-    financialData :
-    {
-        currency : joi.any().valid(currencyList).required(),
-        capital : joi.number().precision(2).required(),
     },
-    investor : 
-    {
-        name : joi.string().min(3).required(),
-        gender : joi.any().valid(genderList).required(),
-        nationality : joi.any().valid(nationalityList).required(),
-        idType : joi.any().valid(idTypeList).required(),
-        id : joi.string().min(3).required(),
-        birthdate : joi.date().required(),
-        telephone : joi.number(),
-        fax : joi.number(),
-        email : joi.string().email(),
-        homeAddress : joi.string().min(3).required()
-    }
-}
+    CompanyName: {
+        type: String,
+        required: true,
+        unique: true
 
-const schema = {
-    regulatingLaw : joi.any().valid(regulatingLawList).required(),
-    companyLegalForm : joi.any().valid(companyLegalFormList).required(),
-    companyName : companyNameSchema,
-    hqInfo : hqInfoSchema,
-    investorInfo : investorInfoSchema
-}
+    },
+    CompanyNationality: {
+        type: [String],
+        required: true,
+        unique: true
 
-// Model class implementation
-class Form{
-    constructor(regulatingLaw, companyLegalForm, companyName, hqInfo, investorInfo){
-        this.id = uuid.v4()
-        this.regulatingLaw = regulatingLawList[regulatingLaw]
-        this.companyLegalForm = companyLegalFormList[companyLegalForm]
-        this.companyName = companyName
-        this.hqInfo = getHqInfo(hqInfo)
-        this.investorInfo = investorInfo
-    }
-    
-    static getSchema(){
-        return schema
-    }
-}
+    },
+    CompanyName_English: {
+        type: String,
+        required: false
+    },
+    Currency: {
+        type: [String],
+        required: true
+    },
+    Capital: {
+        type: Number,
+        required: true
+    },
+    CreatedAt: {
+        type: Date,
+        required: true,
+        default:Date.now
+    },
+    RegulatedLaw: {
+        type: [String],
+        required: true
+    },
+    FormOfLegalCompany: {
+        type: [String],
+        required: true
+    },
+    Governorate: {
+        type: [String],
+        required: true
+    },
+    City: {
+        type: [String],
+        required: true
+    },
+    Address: {
+        type: [String],
+        required: true
+    },
+    Reviewed: {
+        type: Boolean,
+        required: true
+    },
+    Status: {
+        type: Boolean,
+        required: true
+    },
+    Fax: {
+        type: String,
+        required: true
+    },
+    LawyerName: {
+        type: String,
+        required: true
+    },
 
-module.exports = Form
+    InvestorName: {
+        type: [String],
+        required: true
+    },
+    InvestorGender: {
+        type: [String],
+        required: true
+    },
+    InvestorNationality: {
+        type: [String],
+        required: true
+    },
+    InvestorId: {
+        type: [Number],
+        required: true,
+        unique: true
+    },
+    InvestorBirthdate: {
+        type: [Date],
+        required: true
+    },
+    InvestorTelephone: {
+        type: [Number],
+        required: true
+    },
+    InvestorFax: {
+     type: [String],
+     required: true
+    },
+    InvestorEmail: {
+        type: [String],
+        required: true
+    },
+    InvestorHomeAddress: {
+        type: [String],
+        required: true
+    },
+  
+})
+
+module.exports = SpcForm = mongoose.model('SpcForm', SpcFormSchema)
--- models/form/SscForm.js
+++ models/form/SscForm.js
@@ -1,90 +1,145 @@
-// npm imports
-const uuid = require('uuid')
-const joi = require('joi')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
-// Global constants
-// Lists
-const regdulatingLawList = ["law1", "law2"]
-const companyLegalFormList = ["form1", "form2"]
-const governerateList = ["gov1", "gov2"]
-const cityList = ["city1", "city2"]
-const currencyList = ["cur1", "cur2"]
-const genderList = ["g1", "g2"]
-const nationalityList = ["n1", "n2"]
-const idTypeList = ["id1", "id2"]
-const boardPositionList = ["pos1", "pos2"]
-// fields
-const minCapital = 50000
 
-// Schemas
-const companyNameSchema =  {
-    arabic : joi.string().min(3).required(),
-    english : joi.string().min(3)
-}
+const FormSchema = new Schema({
+    id: {
+        type: Number,
+        required: true,
+        unique: true
 
-const hqInfoSchema = {
-    governerate : joi.any().valid(governerateList).required(),
-    city : joi.any().valid(cityList).required(),
-    address : joi.string().min(3).required(),
-    telephone : joi.number(),
-    fax : joi.number()
-}
-
-const investorInfoSchema = {
-    financialData :
-    {
-        currency : joi.any().valid(currencyList).required(),
-        capital : joi.number().min(minCapital).max(999999999999).precision(2).required(),
     },
-    investor : 
-    {
-        name : joi.string().min(3).required(),
-        gender : joi.any().valid(genderList).required(),
-        nationality : joi.any().valid(nationalityList).required(),
-        idType : joi.any().valid(idTypeList).required(),
-        id : joi.string().min(3).required(),
-        birthdate : joi.date().required(),
-        telephone : joi.number(),
-        fax : joi.number(),
-        email : joi.string().email(),
-        homeAddress : joi.string().min(3).required()
-    }
-}
+    CompanyName: {
+        type: String,
+        required: true,
+        unique: true
 
-const boardOfDirectorsSchema = {
-    name : joi.string().min(3).required(),
-    gender : joi.any().valid(genderList).required(),
-    nationality : joi.any().valid(nationalityList).required(),
-    idType : joi.any().valid(idTypeList).required(),
-    id : joi.string().min(3).required(),
-    birthdate : joi.date().required(),
-    homeAddress : joi.string().min(3).required(),
-    position : joi.any().valid(boardPositionList).required(),
-}
+    },
+    CompanyNationality: {
+        type: [String],
+        required: true,
+        unique: true
 
-const schema = {
-    regdulatingLaw : joi.any().valid(regdulatingLawList).required(),
-    companyLegalForm : joi.any().valid(companyLegalFormList).required(),
-    companyName : companyNameSchema,
-    hqInfo : hqInfoSchema,
-    investorInfo : investorInfoSchema,
-    boardOfDirectors : joi.array().items(boardOfDirectorsSchema)
-}
+    },
+    CompanyName_English: {
+        type: String,
+        required: false
+    },
+    Currency: {
+        type: [String],
+        required: true
+    },
+    Capital: {
+        type: Number,
+        min:50000
+    },
+    CreatedAt: {
+        type: Date,
+        required: true,
+        default:Date.now
+    },
+    RegulatedLaw: {
+        type: [String],
+        required: true
+    },
+    FormOfLegalCompany: {
+        type: [String],
+        required: true
+    },
+    Governorate: {
+        type: [String],
+        required: true
+    },
+    City: {
+        type: [String],
+        required: true
+    },
+    Address: {
+        type: [String],
+        required: true
+    },
+    boardOfDirectorsName: {
+        type: [String],
+        required: true
+    },
+    boardOfDirectorsId: {
+        type: [Number],
+        required: true
+    },
+    boardOfDirectorsGender: {
+        type: [String],
+        required: true
+    },
+    boardOfDirectorsNationality: {
+        type: [String],
+        required: true
+    },
+    boardOfDirectorsBirthDate: {
+        type: [Date],
+        required: true
+    },
+    boardOfDirectorsPosition: {
+        type: [String],
+        required: true
+    },
+    boardOfDirectorsHomeAddress: {
+        type: [String],
+        required: true
+    },
+    Reviewed: {
+        type: Boolean,
+        required: true
+    },
+    Status: {
+        type: Boolean,
+        required: true
+    },
+    Fax: {
+        type: String,
+        required: true
+    },
+    LawyerName: {
+        type: String,
+        required: true
+    },
 
-// Model class implementation
-class Form{
-    constructor(regulatingLaw, companyLegalForm, hqInfo, investorInfo, boardOfDirectors){
-        this.id = uuid.v4()
-        this.regdulatingLaw = regulatingLaw
-        this.companyLegalForm = companyLegalForm
-        this.hqInfo = hqInfo
-        this.investorInfo = investorInfo
-        this.boardOfDirectors = boardOfDirectors
-    }
-    
-    static getSchema(){
-        return schema
-    }
-}
+    InvestorName: {
+        type: [String],
+        required: true
+    },
+    InvestorGender: {
+        type: [String],
+        required: true
+    },
+    InvestorNationality: {
+        type: [String],
+        required: true
+    },
+    InvestorId: {
+        type: [Number],
+        required: true,
+        unique: true
+    },
+    InvestorBirthdate: {
+        type: [Date],
+        required: true
+    },
+    InvestorTelephone: {
+        type: [Number],
+        required: true
+    },
+    InvestorFax: {
+     type: [String],
+     required: true
+    },
+    InvestorEmail: {
+        type: [String],
+        required: true
+    },
+    InvestorHomeAddress: {
+        type: [String],
+        required: true
+    },
+})
 
-module.exports = Form
+module.exports = SscForm = mongoose.model('SscForm', SscFormSchema)
