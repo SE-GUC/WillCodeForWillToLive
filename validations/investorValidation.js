11a76925982354545a89e5fa21533c021b0a1a24
hadeel97 <hadeel.khalifa@student.guc.edu.eg>
2019-03-23 22:59:46 +0200

    Update investorValidation.js

--- validations/investorValidation.js
+++ validations/investorValidation.js
@@ -4,15 +4,15 @@ module.exports = {
     const createSchema = {
       email: Joi.string().required(),
       password: Joi.string().required(),
-      typeOfID: Joi.string().required,
-      name: Joi.string().required,
-      nationality: Joi.string().required,
-      capital: Joi.number().required,
-      DOB: Joi.date().required,
-      mobileNumber: Joi.number().required,
-      address: Joi.string().required,
-      faxNumber: Joi.number().required,
-      gender: Joi.string().required
+      typeOfID: Joi.string().required(),
+      name: Joi.string().required(),
+      nationality: Joi.string().required(),
+      capital: Joi.number().required(),
+      DOB: Joi.date().required(),
+      mobileNumber: Joi.number().required(),
+      address: Joi.string().required(),
+      faxNumber: Joi.number().required(),
+      gender: Joi.string().required()
     };
     return Joi.validate(request, createSchema);
   },
