7b5edb95a37d03d83e05820c49bfd0012e60ab67
abirgamal <abiirrgamal@gmail.com>
2019-03-30 17:02:38 +0200

    spczeft

--- routes/api/spcForm.js
+++ routes/api/spcForm.js
@@ -1,191 +1,97 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const router = express.Router();
-const mongoose = require('mongoose')
-//new 
-const nfetch = require('node-fetch')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
 
-const SpcForm = require('../../models/SpcForm');
-const validator = require('../../validations/SpcFormValidation');
-const config = require('../../config/keys')
 
-router.get('/', async (req,res) => {
-    const SpcForms = await SpcForm.find()
-    res.json({data: SpcForms})
+const SpcFormSchema = new Schema({
+    regulatingLaw: {
+        type: String,
+        required: true
+    },
+    companyLegalForm: {
+        type: String,
+        required: true
+    },
+    companyName: {
+        arabic: {
+            type: String,
+            required: true
+        },
+        english: {
+            type: String
+        }
+    },
+    hqInfo: {
+        governorate: {
+            type: String,
+            required: true
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        telephone: {
+            type: String
+        },
+        fax: {
+            type: String
+        }
+    },
+    CreatedAt: {
+        type: Date,
+        required: true,
+        default:Date.now
+    },
+    investorInfo: {
+        capitalCurrency: {
+            type: String,
+            required: true
+        },
+        capital: {
+            type: Number,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        investorType: {
+            type: String,
+            required: true
+        },
+        gender: {
+            type: String
+        },
+        nationality: {
+            type: String,
+            required: true
+        },
+        idType: {
+            type: String
+        },
+        idNumber: {
+            type: String
+        },
+        birthDate: {
+            type: Date
+        },
+        address: {
+            type: String,
+            required: true
+        },
+        telephone: {
+            type: String
+        },
+        fax: {
+            type: String
+        },
+        email: {
+            type: String
+        }
+    },
 })
 
-// Create a SpcForm
-router.post('/', async (req,res) => {
-   try {
-    const isValidated = validator.createValidation(req.body)
-    if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-    const newSpcForm = await SpcForm.create(req.body)
-     //new
-     const investor = req.body.InvestorName
-     const lawyer = req.body.LawyerName
-     const company = req.body.CompanyName
-     const requestBody = {
-         status: 'pending',
-         investor: investor,
-         reviewer: '-',
-         lawyer: lawyer,
-         company_name: company
-     }
-     const response = await nfetch(`http://localhost:3000/api/cases/`,{
-         method: 'POST',
-         body: JSON.stringify(requestBody),
-         headers: { 'Content-Type': 'application/json' }
-     })
-     const jsonResponse = await response.json()
-     console.log(jsonResponse)
-    res.json({msg:'SpcForm was created successfully', data: newSpcForm})
-   }
-   catch(error) {
-    res.status(404).send({error: 'Error, something is off'});
-   }  
-})
-
-router.get('/calculateFees/:id',async (req,res) =>{
-    try{
-        const SpcFormId = req.params.id
-        const SpcFormElement = await SpcForm.findById(SpcFormId)
-        if(!SpcFormElement){
-            res.status(404).send({error: 'can not be Found'});
-        }
-        else{
-
-            var law = SpcFormElement.RegulatedLaw
-            var capital = SpcFormElement.Capital
-            if(law==="Law159"){
-                let gavi = 1/1000 * capital
-                if(gavi <100){
-                    gavi = 100
-                }
-                if(gavi>1000){
-                    gavi = 1000
-                }
-                let notary = 0.25/100 * capital
-                if(notary <10){
-                    notary = 10
-                }
-                if(notary>1000){
-                    notary = 1000
-                }
-                let Commercial = 56
-                let fees = Commercial + gavi + notary
-                res.json({data: fees})
-            }else{
-                let fees = 610
-                res.json({data: fees})
-            }
-        }
-    }catch(error){
-        res.status(404).send({error: 'Error, something is off'});
-    }
-})
-router.get('/:id', async (req, res)=>{
-    try{
-        const SpcFormId = req.params.id
-        const SpcFormElement = await SpcForm.findById(SpcFormId)
-        if(!SpcFormElement){
-            res.status(404).send({error: 'can not be Found'});
-        }
-
-            res.json({data: SpcFormElement})
-        }
-    
-    
-       catch(error){
-        res.status(404).send({error: 'Error, something is off'});
-    }
-})
-
-/*
-router.put('/:id', async (req,res) => {
-    try {
-     const id = req.params.id
-     const SpcForm = await SpcForm.findOne({id})
-     if(!SpcForm) return res.status(404).send({error: 'SpcForm not found'})
-     const isValidated = validator.updateValidation(req.body)
-     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-     const updatedSpcForm = await SpcForm.updateOne(req.body)
-     res.json({msg: 'SpcForm updated successfully'})
-    }
-    catch(error) {
-        console.log(error)
-    }  
- })*/
- router.put('/:id', async (req, res) => {
-    try{
-        const SpcFormId = req.params.id
-        const SpcFormElement = await SpcForm.findById(SpcFormId)
-        if(!SpcFormElement){
-            res.status(404).send({error: 'SpcForm not found'});
-        }
-        const isValidated = validator.updateValidation(req.body)
-        if (isValidated.error) {
-            res.status(400).send({ error: isValidated.error.details[0].message })
-        }
-        const updatedSpcForm = await SpcForm.findByIdAndUpdate(SpcFormId,req.body)
-        res.json({msg: 'update done'})
-    }
-    catch(error){
-        res.status(404).send({error: 'Error, something is off'});
-    }
-})
-
-/*
-router.put('/:id', async (req,res) => {
-    try {
-     const id = req.params.id
-     const SpcForm = await SpcForm.findOne({id})
-     if(!SpcForm) return res.status(404).send({error: 'SpcForm not found'})
-     const isValidated = validator.updateValidation(req.body)
-     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
-     const updatedSpcForm = await SpcForm.updateOne(req.body)
-     res.json({msg: 'SpcForm updated successfully'})
-    }
-    catch(error) {
-        console.log(error)
-    }  
- })*/
- router.put('/:id', async (req, res) => {
-    try{
-        const SpcFormId = req.params.id
-        const SpcFormElement = await SpcForm.findById(SpcFormId)
-        if(!SpcFormElement){
-            res.status(404).send({error: 'SpcForm not found'});
-        }
-        const isValidated = validator.updateValidation(req.body)
-        if (isValidated.error) {
-            res.status(400).send({ error: isValidated.error.details[0].message })
-        }
-        const updatedSpcForm = await SpcForm.findByIdAndUpdate(SpcFormId,req.body)
-        res.json({msg: 'update done'})}
-        catch(error){
-            res.status(404).send({error: 'Error, something is off'});
-        }
-    })
-router.delete('/:id', async (req,res) => {
-    try{
-        const SpcFormId = req.params.id
-        const deletedSpcForm = await SpcForm.findByIdAndRemove(SpcFormId)
-        res.json({msg: 'Done'})
-    }
-    catch(error){
-        res.status(404).send({error: 'Error, something is off'});
-    }
-})
-
-// router.delete('/:id', async (req,res) => {
-//     try{
-//         const SpcFormId = req.params.id
-//         const deletedSpcForm = await SpcForm.findByIdAndRemove(SpcFormId)
-//         res.json({msg: 'Done'})
-//     }
-//     catch(error){
-//         res.status(404).send({error: 'Error, something is off'});
-//     }
-// })
-
- module.exports = router
+module.exports = SpcForm = mongoose.model('SpcForm', SpcFormSchema)
