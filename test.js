4c044294d199c29f1cdb9a9d35e8fbc3f0350383
amrelsafy <amrelsafyy@gmail.com>
2019-03-31 21:56:35 +0200

    Tests on Reviewer sorting tasks by id and creation date

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const TasksTest = require("./tests/task")
+const ReviewerTest = require("./tests/reviewer")
 const db = require('./config/keys').mongoURI
 
 
@@ -9,6 +10,8 @@ mongoose
     .catch(err => console.log(err))
 
 const task = new TasksTest(3000,'task')
+const reviewer = new ReviewerTest(3000,'reviewer')
 
 Promise.all([task.runIndependently()]).then(result => {})
+Promise.all([reviewer.runIndependently()]).then(result => {})
 
