729ad8876355d513f91e462eabc6a2ad8f8f2f24
Ahmed Khalifa <medoduh@gmail.com>
2019-04-05 20:35:06 +0200

    Finished static form

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import Director from './Director'
-import './Form.css'
 
 class Form extends Component {
   
@@ -19,58 +18,61 @@ constructor(props) {
   }
 
   createForm = () => {
-    const formDiv = document.getElementById('form')
-    const staticForm = formDiv.querySelector("#static_form")
-    const directorDivs = formDiv.querySelector("#boardOfDirectors").querySelectorAll('.director')
-  
     let form = {}
-    // Adding static input
-    for(let input of staticForm.querySelectorAll('input')) {
-      form[input.name] = input.value
-    }
-    // Adding board of directors
-    form.boardOfDirectors = []
-    for(let directorDiv of directorDivs){
-      let director = {}
-      for(let input of directorDiv.querySelectorAll('input')) {
-        director[input.name] = input.value
+    const divs = document.getElementById('static_form').querySelectorAll('div')
+    for(let div of divs) {
+      let subForm = {}
+      for(let input of div.querySelectorAll('input')){
+        subForm[input.name] = input.value
       }
-      form.boardOfDirectors.push(director)
+      form[div.id] = subForm
     }
-    // TODO: Send a post request to backend
+    const directorsDiv = document.getElementById('boardOfDirectors')
+    let directors = []
+    for(let div of directorsDiv.querySelectorAll('div')) {
+      let subForm = {}
+      for(let input of div.querySelectorAll('input')){
+        subForm[input.name] = input.value
+      }
+      directors.push(subForm)
+    }
+    form.boardOfDirectors = directors
+    console.log(form)
   }
 
   render() {
     return (
       <div>
         <div id="form">
-        <div id="static_form">
-          <input type="text" name="RegulatingLaw" placeholder="Regulating Law"/>
-          <input type="text" name="companyType" placeholder="Company Type"/>
-          <div id="companyName">
-            <input type="text" name="companyNameArabic" placeholder="Company Name Arabic"/>
-            <input type="text" name="companyNameEnglish" placeholder="Company Name English"/>
-          </div>
-          <div id="hqInfo">
-            <input type="text" name="hqGovernorate" placeholder="Governorate"/>
-            <input type="text" name="hqCity" placeholder="City"/>
-            <input type="text" name="hqTelephone" placeholder="Telephone"/>
-            <input type="text" name="hqFax" placeholder="Fax"/>
-          </div>
-          <div id="investorInfo">
-            <input type="text" name="capitalCurrency" placeholder="Capital Currency"/>
-            <input type="text" name="capital" placeholder="Capital"/>
-            <input type="text" name="investorName" placeholder="Name"/>
-            <input type="text" name="investorType" placeholder="Type"/>
-            <input type="text" name="investorGender" placeholder="Gender"/>
-            <input type="text" name="investorNationality" placeholder="Nationality"/>
-            <input type="text" name="investorIdType" placeholder="ID Type"/>
-            <input type="text" name="investorBirthdate" placeholder="Birth Date"/>
-            <input type="text" name="investorTelephone" placeholder="Telephone"/>
-            <input type="text" name="investorFax" placeholder="Fax"/>
-            <input type="text" name="investorEmail" placeholder="Email"/>
-            <input type="text" name="investorAddress" placeholder="Address"/>
-          </div>
+          <div id="static_form">
+            <div id="companyInfo">
+              <input type="text" name="RegulatingLaw" placeholder="Regulating Law"/>
+              <input type="text" name="companyType" placeholder="Company Type"/>
+            </div>
+            <div id="companyName">
+              <input type="text" name="arabic" placeholder="Company Name Arabic"/>
+              <input type="text" name="english" placeholder="Company Name English"/>
+            </div>
+            <div id="hqInfo">
+              <input type="text" name="governorate" placeholder="Governorate"/>
+              <input type="text" name="city" placeholder="City"/>
+              <input type="text" name="telephone" placeholder="Telephone"/>
+              <input type="text" name="fax" placeholder="Fax"/>
+            </div>
+            <div id="investorInfo">
+              <input type="text" name="capitalCurrency" placeholder="Capital Currency"/>
+              <input type="text" name="capital" placeholder="Capital"/>
+              <input type="text" name="name" placeholder="Name"/>
+              <input type="text" name="type" placeholder="Type"/>
+              <input type="text" name="gender" placeholder="Gender"/>
+              <input type="text" name="nationality" placeholder="Nationality"/>
+              <input type="text" name="idType" placeholder="ID Type"/>
+              <input type="text" name="birthdate" placeholder="Birth Date"/>
+              <input type="text" name="telephone" placeholder="Telephone"/>
+              <input type="text" name="fax" placeholder="Fax"/>
+              <input type="text" name="email" placeholder="Email"/>
+              <input type="text" name="address" placeholder="Address"/>
+            </div>
           </div>
           <div id="boardOfDirectors">
             <button id="addDirector" onClick={this.addDirector}>Add Director</button>
