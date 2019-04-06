8889cc8409a6cd6d30d4aeed16b78d272fb335aa
Amr-ElNahas <amrtea.edu@gmail.com>
2019-04-06 20:34:45 +0200

    first attempt

---
+++ client/src/components/Cases.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Cases extends Component {
+    state={
+        cases:[]
+    }
+  render() {
+    return (
+      <div className="Cases">
+      <h1>Cases</h1>
+      </div>
+    );
+  }
+}
+
+export default Cases;
