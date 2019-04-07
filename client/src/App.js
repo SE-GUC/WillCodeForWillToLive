2cd5ea5990013934602f185cec0843a8899a980c
abirgamal <abiirrgamal@gmail.com>
2019-04-07 17:35:25 +0200

    added company and investor user story

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Form from './components/Form'
+import Companies from './components/Companies';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <Router>
         <Switch>
           <Route path="/Form" component={Form} />
+          <Route path="/Companies" component={Companies} />
         </Switch>
       </Router>
     );
