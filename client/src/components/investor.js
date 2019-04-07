7db79c4a1348c6df1c96fc8f7c94505d63c6239a
hadeel97 <hadeel.khalifa@student.guc.edu.eg>
2019-04-07 23:32:12 +0200

    done

--- client/src/components/investor.js
+++ client/src/components/investor.js
@@ -4,11 +4,12 @@
 
   class investor extends Component {
       state={
-          investor:[]
+          investor:[],
+          username: "oooe"
       }
       
       componentDidMount(){
-       axios.get('http://localhost:3002/api/investor').then(res => Object.values(res)[0]).then(element => this.setState({investor:element.data}))
+       axios.get('http://localhost:3002/api/investor/getCases/'+this.state.username).then(res => Object.values(res)[0]).then(element => this.setState({investor:element.data}))
       }
     render() {
       return(
@@ -26,7 +27,7 @@
             {this.state.investor.map((investor)=> {
               return(
                 <tr>
-                <td>{<h5><div>InvestorUsername: {investor._id}</div><div> Case Status: {investor.status}</div><div> Investor: {investor.investor}</div></h5>}</td>
+                <td>{<h5><div>CaseID: {investor._id}</div><div> Case Status: {investor.status}</div><div> Investor: {investor.investor}</div><div> Reviewer:  {investor.reviewer}</div><div> Lawyer: {investor.lawyer}</div><div>Company Name: {investor.company_name}</div><div> reviewed by lawyer:  {investor.reviewed_by_lawyer}</div><div> Lawyer Comment: {investor.review_comment_by_lawyer}</div><div> lawyer comment date: {investor.review_date_by_lawyer}</div><div> reviewed by reviewer: {investor.reviewed_by_reviewer}</div><div> reviewer comment:  {investor.review_comment_by_reviewer}</div><div>reviewer comment date:  {investor.review_date_by_reviewer}</div></h5>}</td>
                 </tr>
               )
             })
