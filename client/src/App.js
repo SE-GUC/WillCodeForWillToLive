9df2b57785e1d1079806cc6b0bf5fd22669a3ad5
OmarMorsi <omarmorsy77@yahoo.com>
2019-04-07 21:28:57 +0200

    Displayed all Cases for the FrontEnd

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import RegisterLawyer from './components/pages/RegisterLawyer';
 import RegisterReviewer from './components/pages/RegisterReviewer';
 import Form from './components/Form'
+import lawyer from './components/lawyer'
 class App extends Component {
   render() {
     return (
@@ -18,7 +19,10 @@ class App extends Component {
        {
                 <Route path="/registerReviewer" component={RegisterReviewer} />
           }
-       
+          {
+            <Route path="/Lawyer" component = {lawyer} />
+          }
+
 
         </Switch>
       </Router>
