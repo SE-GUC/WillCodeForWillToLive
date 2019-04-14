8742526ec85c2fa14bc048d52e8474bd0a6bc248
amrelsafy <amrelsafyy@gmail.com>
2019-04-14 11:47:49 +0200

    Added modifications to case backend

--- models/Case.js
+++ models/Case.js
@@ -45,7 +45,44 @@ const caseSchema = new Schema({
     review_date_by_reviewer: {
         type: Date,
         required: false
+    },
+    fees: {
+       type: Number,
+       required: false
+   },
+    paid: {
+        type: Boolean,
+        required: false
+    },
+    currency: {
+        type: String,
+        required: false
+    },
+    formID: {
+        type: String,
+        required: false
+    },
+    priority: {
+        type : String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    created_at: {
+        type: String,
+        required: true
+    },
+    isDone: {
+        type: Boolean,
+        required: true
+    },
+    dueDate: {
+        type: String,
+        required: true
     }
-   
 })
 module.exports = Case = mongoose.model('Case',caseSchema)
