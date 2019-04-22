8a0677512d66d111cb7cd96987e5636d1020d05d
Ahmed Khalifa <medoduh@gmail.com>
2019-04-22 22:47:33 +0200

    Added fees payment

--- client/src/components/lawyerCases.js
+++ client/src/components/lawyerCases.js
@@ -22,9 +22,20 @@ import React, { Component } from 'react';
       this.setState({ lawyerCases: this.state.lawyerCases })
    
    }
-  
 
-      
+   payFees = (id) => {
+    let _case = null
+    axios.get(`/api/cases/${id}`)
+    .then(({case2}) => _case = case2)
+    .catch(error => alert(error))
+    if(!_case) {
+      return
+    }
+    const newState = this.state.lawyerCases
+    newState.splice(newState.findIndex(({_id})=>_id === id), 1, _case)
+    this.setState({lawyerCases: newState})
+   } 
+  
       componentDidMount(){
        
        axios.get('http://localhost:3002/api/lawyer/getCases/'+this.state.username).then(res => Object.values(res)[0]).then(element => this.setState({lawyerCases:element.data}))
@@ -46,26 +57,29 @@ import React, { Component } from 'react';
             { this.state.lawyerCases.map((lawyerCases) => {
               return(
                 <tr>
-                  <td>{<h5><div>CaseID: {lawyerCases._id}</div>
-                  <div> Case Status: {lawyerCases.status}</div>
-                  <div> Investor: {lawyerCases.investor}</div>
-                  <div> Reviewer:  {lawyerCases.reviewer}</div>
-                  <div> Lawyer: {lawyerCases.lawyer}</div>
-                  <div>Company Name: {lawyerCases.company_name}</div>
-                  <div> reviewed by lawyer:  {lawyerCases.reviewed_by_lawyer}</div>
-                  <div> Lawyer Comment: {lawyerCases.review_comment_by_lawyer}</div>
-                  <div> lawyer comment date: {lawyerCases.review_date_by_lawyer}</div>
-                  <div> reviewed by reviewer: {lawyerCases.reviewed_by_reviewer}</div>
-                  <div> reviewer comment:  {lawyerCases.review_comment_by_reviewer}</div>
-                  <div>reviewer comment date:  {lawyerCases.review_date_by_reviewer}</div>
-                  <div> fees: {lawyerCases.fees}</div><div> paid:  {lawyerCases.paid}</div>
-                  <div> currency: {lawyerCases.currency}</div>
-                  <div>formID: {lawyerCases.formID}</div>
-                  <div> priority:  {lawyerCases.priority}</div>
-                  <div> description: {lawyerCases.description}</div>
-                  <div> created_at: {lawyerCases.created_at}</div>
-                  <div> isDone: {lawyerCases.isDone}</div>
-                  <div> dueDate:  {lawyerCases.dueDate}</div></h5>}</td>
+                  <td>
+                    {<h5><div>CaseID: {lawyerCases._id}</div>
+                    <div> Case Status: {lawyerCases.status}</div>
+                    <div> Investor: {lawyerCases.investor}</div>
+                    <div> Reviewer:  {lawyerCases.reviewer}</div>
+                    <div> Lawyer: {lawyerCases.lawyer}</div>
+                    <div>Company Name: {lawyerCases.company_name}</div>
+                    <div> reviewed by lawyer:  {lawyerCases.reviewed_by_lawyer}</div>
+                    <div> Lawyer Comment: {lawyerCases.review_comment_by_lawyer}</div>
+                    <div> lawyer comment date: {lawyerCases.review_date_by_lawyer}</div>
+                    <div> reviewed by reviewer: {lawyerCases.reviewed_by_reviewer}</div>
+                    <div> reviewer comment:  {lawyerCases.review_comment_by_reviewer}</div>
+                    <div>reviewer comment date:  {lawyerCases.review_date_by_reviewer}</div>
+                    <div> fees: {lawyerCases.fees}</div><div> paid:  {lawyerCases.paid}</div>
+                    <div> currency: {lawyerCases.currency}</div>
+                    <div>formID: {lawyerCases.formID}</div>
+                    <div> priority:  {lawyerCases.priority}</div>
+                    <div> description: {lawyerCases.description}</div>
+                    <div> created_at: {lawyerCases.created_at}</div>
+                    <div> isDone: {lawyerCases.isDone}</div>
+                    <div> dueDate:  {lawyerCases.dueDate}</div></h5>}
+                    <button onClick={_=>payFees(lawyerCases._id)}>Pay Fees</button>
+                  </td>
                 </tr>
               )
             })}
